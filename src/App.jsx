5fbb1fcbb91e780c2bc4965c6b8dd917bbf43774
anshumann01 <anshumankrishna01@gmail.com>
2026-02-12 19:44:20 +0530

    resolve README conflict

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './Components/navbar.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
